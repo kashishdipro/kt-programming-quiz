d559be8e21ea2a7adcb507880bfff6a4a73d879d
kashishdipro <kamanashishdatta@gmail.com>
2022-10-23 00:06:18 +0600

    final commit for assignment

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -8,7 +8,7 @@ const Statistics = () => {
     console.log(data);
     return (
         <section className='bg-cyan-50 mt-16 lg:px-32 md:px-16 px-4 py-5'>
-            <div className="pt-32">
+            <div className="pt-36">
             <h2 className='text-2xl font-bold text-center text-slate-500 mb-2'>This is the name and total statistics of quiz:</h2>
                 <LineChart width={350} height={400} data={data}>
                     <Line type="monotone" dataKey="total" stroke="#82ca9d"/>
